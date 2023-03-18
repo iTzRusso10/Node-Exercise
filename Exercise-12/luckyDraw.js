fdb372e0abed5ec240a9e8f3be7b37ca04d61f78
Giuseppe <giusepperusso1003@gmail.com>
2023-03-18 03:49:33 +0100

    Exercise-12

---
+++ Exercise-12/luckyDraw.js
@@ -0,0 +1,39 @@
+const clc = require('cli-color');
+const { agent } = require('supertest');
+
+const luckyDraw = (player) => {
+    return new Promise((resolve, reject) => {
+      const win = Boolean(Math.round(Math.random()));
+  
+      process.nextTick(() => {
+        if (win) {
+          resolve(clc.green(`${player} won a prize in the draw!`));
+        } else {
+          reject(clc.red((`${player} lost the draw.`)));
+        }
+      });
+    });
+  }
+
+  luckyDraw("Joe")
+  .then(res => console.log(res))
+  .catch(rej => console.log(rej))
+  luckyDraw("Sabrina")
+  .then(res => console.log(res))
+  .catch(rej => console.log(rej))
+  luckyDraw("Caroline")
+  .then(res => console.log(res))
+  .catch(rej => console.log(rej))
+
+  const agetResult = async (name) => {
+    try{
+      const res = await luckyDraw(name);
+      console.log(res)
+    }catch(err){
+      console.log(err)
+    }
+  }
+
+ agetResult("Tina")
+ agetResult("Jorge")
+ agetResult("Julien")
